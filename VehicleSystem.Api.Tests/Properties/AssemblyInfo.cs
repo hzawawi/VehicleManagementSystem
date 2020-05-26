e30a93527a6a5008301b5a9fb801590e7926339b
hzawawi <hussein.zawawi@gmail.com>
2020-05-26 20:32:53 +1200

    Add some integration tests

---
+++ VehicleSystem.Api.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VehicleSystem.Api.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VehicleSystem.Api.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b56162d6-91a2-42a5-b37b-4858b32c8125")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VehicleSystem.Api.Tests/VehicleControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using SpecLight;
+using VehicleSystem.Api.Controllers;
+using VehicleSystem.Core.Services;
+using Xunit;
+
+namespace VehicleSystem.Api.Tests
+{
+    public class VehicleControllerTests
+    {
+        private VehicleController _controller;
+        private long _vehicleId;
+
+        [Fact]
+        public void CreateVehicleShouldReturnId()
+        {
+            new Spec("Create a new vehicle")
+                .Given(RequestReceived)
+                .When(CreateIsCalled)
+                .Then(VehicleIsCreated)
+                .Execute();
+        }
+
+        public void RequestReceived()
+        {
+            _controller = new VehicleController(new VehicleService());
+        }
+
+        public void CreateIsCalled()
+        {
+            _vehicleId = _controller.Create();
+        }
+
+
+        public void VehicleIsCreated()
+        {
+            Assert.True( _vehicleId != 0);
+        }
+    }
+}
